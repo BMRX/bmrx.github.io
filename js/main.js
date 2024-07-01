851dfc205c521b7f377c14c40211d617ff2ef83b
Mike Bergstrom <mikeberg.it@gmail.com>
2024-06-30 18:52:47 -0700

    Huge changes, huge!
    Good news everyone!

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.height = "50px";
+    document.getElementById("header_content").style.height = "50px";
+    document.getElementById("site_name").style.fontSize = "15px";
   } else {
-    document.getElementById("header").style.height = "100px";
+    document.getElementById("header_content").style.height = "100px";
+    document.getElementById("site_name").style.fontSize = "30px";
   }
 } 
